506bd874360e607fa491b2eaa6225ddbacf828cf
ytrsk <2232160811@qq.com>
2019-11-25 13:04:40 +0800

    update

--- project/vivado/Exp/Exp1/Exp1-1/Exp1-1-andgate/Exp1-1-andgate.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\StudyAndWork\\VIVADO\\VIVADO\\Vivado\\2015.2\\bin
-call %xv_path%/xelab  -wto cd0e03192dd04868bf843f54e8d24a60 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot s_behav xil_defaultlib.s xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- project/vivado/Exp/Exp1/Exp1-1/Exp1-1-ornotgate/Exp1-1-ornotgate.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\StudyAndWork\\VIVADO\\VIVADO\\Vivado\\2015.2\\bin
-call %xv_path%/xsim s_behav -key {Behavioral:sim_1:Functional:s} -tclbatch s.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
