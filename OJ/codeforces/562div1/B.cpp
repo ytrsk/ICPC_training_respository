3a1f9798f38e632be4d5d22106dee0cb31638aa6
ytrsk <2232160811@qq.com>
2020-04-15 00:38:03 +0800

    Codeforces-1168B.cpp

--- OJ/codeforces/562div1/B.cpp
+++ OJ/codeforces/562div1/B.cpp
@@ -13,7 +13,26 @@ int read(){
     while(ch>='0'&&ch<='9') x=x*10+ch-'0',ch=getchar();
     return x*f;
 }
+char s[300007];
+int val[maxn];
 int main(){
-    
+    scanf("%s",s+1);
+    int len=strlen(s+1);
+    for(int i=1;i<=len;i++) val[i]=0;
+    for(int i=1;i<=len;i++){
+        for(int k=1;k<=10;k++){
+            if(i-k>=1&&i+k<=len){
+                if(s[i-k]==s[i+k]&&s[i-k]==s[i]){
+                    val[i+k]=max(val[i+k],i-k);
+                }
+            }
+        }
+    }
+    int now=0;ll ans=0;
+    for(int i=1;i<=len;i++){
+        now=max(now,val[i]);
+        ans+=now;
+    }
+    cout<<ans;
     return 0;
 }
